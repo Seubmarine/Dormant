d00319c1c5124032db6de2b834e319c6d084f3d5
Seubmarine <Seubmarinepro@gmail.com>
2021-01-27 06:24:53 +0100

    Mouse can escape game window when pressing escape

--- src/game/scripts/game.gd
+++ src/game/scripts/game.gd
@@ -4,6 +4,7 @@ func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 onready var is_fullscreen := false
+onready var is_captured := true
 func _unhandled_input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("fullscreen"):
 		if is_fullscreen:
@@ -11,3 +12,10 @@ func _unhandled_input(_event: InputEvent) -> void:
 		else:
 			is_fullscreen = true
 		OS.window_fullscreen = is_fullscreen
+	if Input.is_action_just_pressed("ui_cancel"):
+		is_fullscreen = !is_fullscreen
+		print(is_fullscreen)
+		if is_fullscreen:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
